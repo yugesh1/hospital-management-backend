53c677f1ad08e664aa11f60e1d9f6d1e23fab5cf
yugesh1 <murugesanyugesh@gmail.com>
2023-09-20 19:06:04 +0530

    cookie domain added

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,6 +6,7 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     httpOnly: true,
+    domain: ".herokuapp.com",
   };
 
   res.cookie("token", token, options).status(statusCode).json({
